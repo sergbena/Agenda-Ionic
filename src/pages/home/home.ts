db2bc9456dd51fae3522f50faf223b58fa94d11a
Sergio Benavides <sergiobv1@gmail.com>
2018-12-16 18:08:21 +0100

    add agenda and segments

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +7,42 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alert: AlertController) {
+
+  }
+
+  add(){
+
+    let choice = this.alert.create({
+      title : 'Añadir',
+      inputs: [{
+        type: 'radio',
+        label: 'Lista',
+        value: 'list'
+      },{
+        type: 'radio',
+        label: 'Notas',
+        value: 'notes'
+      },{
+        type: 'radio',
+        label: 'Contacto',
+        value: 'contact'
+      }],
+      buttons: [{
+        text: 'Cancel',
+        role: 'cancel',
+        handler: data=>{
+          console.log('Cancel');
+        }
+      },{
+        text: 'Acept',
+        handler: data=>{
+          console.log(data);
+        }
+      }]
+    });
+
+    choice.present();
 
   }
 
